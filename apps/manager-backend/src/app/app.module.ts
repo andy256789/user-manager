fa67a41b3b3759d06ab0efdcaad7d23d823453c1
andy256789 <andonovatanq3@gmail.com>
2025-07-26 19:38:29 +0300

    Updating dockerfile and package.json for bcrypt integration

--- apps/manager-backend/src/app/app.module.ts
+++ apps/manager-backend/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from '../auth/auth.module';
@@ -7,7 +9,16 @@ import { PersonModule } from '../person/person.module';
 import { DepartmentModule } from '../department/department.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, PersonModule, DepartmentModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    PersonModule,
+    DepartmentModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
